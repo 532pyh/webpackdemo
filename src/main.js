50a8ce3e44c6ac2e28156493b49fa2e5423ef8c9
pengyh <pt370390794@126.com>
2020-06-26 07:01:32 +0800

    update .gitignore

---
+++ src/main.js
@@ -0,0 +1,3 @@
+import './css/index.css?inline'
+import './ass/main.css?inline'
+console.log(1)
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    mode: "production",
+    entry: {
+        main: "./src/main.js",
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin()
+    ],
+    optimization: {
+        minimize: false,
+    },
+    module: {
+        rules: [
+            {
+                resourceQuery: /inline/,
+                use: [
+                    {
+                        loader:'style-loader',
+                    },
+                    {
+                        loader:'css-loader'
+                    }
+                ],
+            },
+        ],
+    },
+};
